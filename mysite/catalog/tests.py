736a15ec986eec6ad17dcfd1b461056d484bd8bf
Luganskaya Sveta <lugansckajas@yandex.ru>
2022-10-26 15:17:53 +0300

    Improve tests

--- mysite/catalog/tests.py
+++ mysite/catalog/tests.py
@@ -6,30 +6,40 @@ class StaticURLTests(TestCase):
         response = Client().get('/catalog/')
         self.assertEqual(response.status_code, 200)
 
-    # Все следующие тесты проверяют корректность работы регулярки:
 
+class RegexTests(TestCase):
     def test_catalog_normal_num_endpoint(self):
         responses = ['/catalog/123/', '/catalog/12/', '/catalog/1/']
-        for response in responses:
-            response = Client().get(response)
-            self.assertEqual(response.status_code, 200)
+        for resp in responses:
+            with self.subTest(resp=resp):
+                response = Client().get(resp)
+                self.assertEqual(response.status_code, 200)
+
+    def test_unormal_num_endpoint(self):
+        responses = ['/catalog/0/', '/catalog/01', '/catalog/000/',
+                     'catalog/-1/']
+        for resp in responses:
+            with self.subTest(resp=resp):
+                response = Client().get(resp)
+                self.assertEqual(response.status_code, 404)
 
     def test_catalog_mixed_endpoint(self):
-        response = Client().get('/catalog/1s/')
-        self.assertEqual(response.status_code, 404)
+        responses = ['/catalog/12s/', '/catalog/s12', '/catalog/0a/',
+                     'catalog/a0/', 'catalog/a0a/', 'catalog/1a0/',
+                     '/catalog/1\n/', '/catalog/\n1/', '/catalog/1\n1/',
+                     '/catalog/s\n1/', '/catalog/123\n1/', '/catalog/\t1/',
+                     '/catalog/1\t/', '/catalog/1 ']
+        for resp in responses:
+            with self.subTest(resp=resp):
+                response = Client().get(resp)
+                self.assertEqual(response.status_code, 404)
 
     def test_catalog_str_endpoint(self):
-        response = Client().get('/catalog/s/')
-        self.assertEqual(response.status_code, 404)
-
-    def test_catalog_zero_endpoint(self):
-        response = Client().get('/catalog/0/')
-        self.assertEqual(response.status_code, 404)
-
-    def test_catalog_start_zero_endpoint(self):
-        response = Client().get('/catalog/01/')
-        self.assertEqual(response.status_code, 404)
-
-    def test_catalog_negative_num_endpoint(self):
-        response = Client().get('/catalog/-1/')
-        self.assertEqual(response.status_code, 404)
+        responses = ['/catalog/s/', '/catalog/s\n', '/catalog/\n/',
+                     '/catalog/a0/', '/catalog/a0a/', '/catalog/1a0/',
+                     '/catalog/\t/', '/catalog/\tsv/', '/catalog/sv\t/',
+                     '/catalog/\n\t/', '/catalog/    /']
+        for resp in responses:
+            with self.subTest(resp=resp):
+                response = Client().get(resp)
+                self.assertEqual(response.status_code, 404)
--- mysite/homepage/tests.py
+++ mysite/homepage/tests.py
@@ -5,7 +5,3 @@ class StaticURLTests(TestCase):
     def test_homepage_endpoint(self):
         response = Client().get('/')
         self.assertEqual(response.status_code, 200)
-
-    def test_not_homepage_endpoint(self):
-        response = Client().get('/123')
-        self.assertEqual(response.status_code, 404)
