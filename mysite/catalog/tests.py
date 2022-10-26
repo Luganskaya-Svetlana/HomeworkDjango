8329243ad55b9d61ef42135acb590552582e0d74
Luganskaya Sveta <lugansckajas@yandex.ru>
2022-10-26 15:49:05 +0300

    Add some new tests and delete tests that cannot be reapeted by the user

--- mysite/catalog/tests.py
+++ mysite/catalog/tests.py
@@ -26,19 +26,18 @@ class RegexTests(TestCase):
     def test_catalog_mixed_endpoint(self):
         responses = ['/catalog/12s/', '/catalog/s12', '/catalog/0a/',
                      'catalog/a0/', 'catalog/a0a/', 'catalog/1a0/',
-                     '/catalog/1\n/', '/catalog/\n1/', '/catalog/1\n1/',
-                     '/catalog/s\n1/', '/catalog/123\n1/', '/catalog/\t1/',
-                     '/catalog/1\t/', '/catalog/1 ']
+                     '/catalog/1-1/', '/catalog/1+1/', '/catalog/1*1/',
+                     '/catalog/*s1/', '/catalog/123*1s/', '/catalog/+1/',
+                     '/catalog/*1/', '/catalog/1*']
         for resp in responses:
             with self.subTest(resp=resp):
                 response = Client().get(resp)
                 self.assertEqual(response.status_code, 404)
 
     def test_catalog_str_endpoint(self):
-        responses = ['/catalog/s/', '/catalog/s\n', '/catalog/\n/',
-                     '/catalog/a0/', '/catalog/a0a/', '/catalog/1a0/',
-                     '/catalog/\t/', '/catalog/\tsv/', '/catalog/sv\t/',
-                     '/catalog/\n\t/', '/catalog/    /']
+        responses = ['/catalog/s/', '/catalog/s ', '/catalog/-abc/',
+                     '/catalog/*/', '/catalog/+/', '/catalog/-/',
+                     '/catalog/    /']
         for resp in responses:
             with self.subTest(resp=resp):
                 response = Client().get(resp)
