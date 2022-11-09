825890b051c9c10722875862ac9245e11aceb604
Luganskaya Sveta <lugansckajas@yandex.ru>
2022-11-09 20:14:34 +0300

    Add footer and pages for about and catalog

--- mysite/catalog/views.py
+++ mysite/catalog/views.py
@@ -1,8 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def item_list(request):
-    return HttpResponse('Список прекрасных элементов')
+    template_name = 'catalog/item_list.html'
+    return render(request, template_name)
+
+
+# def item_list(request):
+#     return HttpResponse('Список прекрасных элементов')
 
 
 def item_detail(request, pk):
